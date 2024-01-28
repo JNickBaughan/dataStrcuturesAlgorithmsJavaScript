1a01d59883905d4c431eed4d5cedc47f18ccb7c5
J. Nick Baughan <JNickBaughan@Gmail.com>
2024-01-28 10:30:07 -0500

    WIP - tree structure

---
+++ Tree/index.ts
@@ -0,0 +1,19 @@
+// implement TreeNode
+class TreeNode<T> {
+
+    private _data: T | undefined;
+    private _children: TreeNode<T>[];
+
+    constructor(data: T) {
+
+        this._data = data;
+
+        this._children = []
+    }
+
+    add(data: T) {
+        const node = new TreeNode(data);
+        this._children = [ node, ...this._children]
+    }
+
+}
